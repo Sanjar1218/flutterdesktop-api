29a3846c2d36b94352e418ee3d7d3f4d24969072
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-25 17:30:12 +0500

    can send image as bytstring

--- img_app/views.py
+++ img_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import cv2
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def home(request):
+    f = request.FILES
+    print(f)
+    img = f['img'].read()
+    with open('img.png', 'wb') as f:
+        f.write(img)
+        f.close()
+    
+    img = cv2.imread('img.png')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    cv2.imwrite('gray.png', gray)
+    with open('gray.png', 'rb') as f:
+        content = f.read()
+    print(content)
+    return Response({"status":str(content)})
